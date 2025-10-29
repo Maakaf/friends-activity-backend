2aa0727abe58e3ad64a3dfbd4f4f75a0ec7c804c
Sivan Becker <sivanbecker@gmail.com>
2025-10-29 09:54:41 +0200

    Add unit tests for PipelineService to validate user input handling
    - Introduced a new test suite for PipelineService with tests for methods: run, generateReport, removeUsers, and addNewUsers.
    - Each method test checks for proper error handling when the users array is empty, ensuring a BadRequestException is thrown with the correct message.

---
+++ src/pipeline/__tests__/pipeline.service.spec.ts
@@ -0,0 +1,111 @@
+import { BadRequestException } from '@nestjs/common';
+import { Test, TestingModule } from '@nestjs/testing';
+import { MockMetadata, ModuleMocker } from 'jest-mock';
+import { DataSource } from 'typeorm';
+import { AnalyticsReportService } from '../../analytics/analytics-report.service';
+import { AnalyticsService } from '../../analytics/analytics.service';
+import { SilverOrchestratorService } from '../../normalized/orchestrator';
+import { GithubService } from '../../raw/raw.service';
+import { PipelineService } from '../pipeline.service';
+
+const moduleMocker = new ModuleMocker(global);
+
+
+describe('PipelineService', () => {
+    let service: PipelineService;
+    let mockDataSource: DataSource;
+    let nestTestmodule: TestingModule;
+
+    const createService = async () => {
+        const mockDataSourceValue = moduleMocker.generateFromMetadata(
+            moduleMocker.getMetadata(DataSource) as MockMetadata<any, any>
+        );
+        moduleMocker.getMetadata(DataSource) as MockMetadata<any, any>;
+        const mockGithubServiceValue = moduleMocker.generateFromMetadata(
+            moduleMocker.getMetadata(GithubService) as MockMetadata<any, any>
+        );
+        const mockSilverOrchestratorServiceValue = moduleMocker.generateFromMetadata(
+            moduleMocker.getMetadata(SilverOrchestratorService) as MockMetadata<any, any>
+        );
+        const mockAnalyticsServiceValue = moduleMocker.generateFromMetadata(
+            moduleMocker.getMetadata(AnalyticsService) as MockMetadata<any, any>
+        );
+        const mockAnalyticsReportServiceValue = moduleMocker.generateFromMetadata(
+            moduleMocker.getMetadata(AnalyticsReportService) as MockMetadata<any, any>
+        );
+
+
+        nestTestmodule = await Test.createTestingModule({
+            providers: [
+                PipelineService,
+                {
+                    provide: DataSource,
+                    useValue: mockDataSourceValue,
+                },
+                {
+                    provide: GithubService,
+                    useValue: mockGithubServiceValue,
+                },
+                {
+                    provide: SilverOrchestratorService,
+                    useValue: mockSilverOrchestratorServiceValue,
+                },
+                {
+                    provide: AnalyticsService,
+                    useValue: mockAnalyticsServiceValue,
+                },
+                {
+                    provide: AnalyticsReportService,
+                    useValue: mockAnalyticsReportServiceValue,
+                },
+
+
+            ],
+        }).compile();
+
+        service = nestTestmodule.get<PipelineService>(PipelineService);
+        mockDataSource = nestTestmodule.get<DataSource>(DataSource);
+    };
+    afterEach(async () => {
+        if (nestTestmodule) {
+            await nestTestmodule.close();
+        }
+    });
+    describe('run', () => {
+        it('Should throw if users array is empty', async () => {
+            await createService();
+            await expect(service.run([]))
+                .rejects
+                .toThrow('Body must be { "users": string[] } with at least one username.');
+            await expect(service.run([])).rejects.toBeInstanceOf(BadRequestException);
+        });
+    });
+    describe('generateReport', () => {
+        it('Should throw if users array is empty', async () => {
+            await createService();
+            await expect(service.generateReport([]))
+                .rejects
+                .toThrow('Body must be { "users": string[] } with at least one username.');
+            await expect(service.generateReport([])).rejects.toBeInstanceOf(BadRequestException);
+        });
+    });
+    describe('removeUsers', () => {
+        it('Should throw if users array is empty', async () => {
+            await createService();
+            await expect(service.removeUsers([]))
+                .rejects
+                .toThrow('Body must be { "users": string[] } with at least one username.');
+            await expect(service.removeUsers([])).rejects.toBeInstanceOf(BadRequestException);
+        });
+    });
+    describe('addNewUsers', () => {
+        it('Should throw if users array is empty', async () => {
+            await createService();
+            await expect(service.addNewUsers([]))
+                .rejects
+                .toThrow('Body must be { "users": string[] } with at least one username.');
+            await expect(service.addNewUsers([])).rejects.toBeInstanceOf(BadRequestException);
+        });
+    });
+    describe('listUsers', () => { });
+});
